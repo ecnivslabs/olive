559d8f0fcabaea2000482ee1aea64e0440fbb070
vinceswu <vinceswu88@gmail.com>
2026-07-19 05:02:48 +0530

    remove parity/

--- parity/02_dict_config.py
+++
@@ -1,4 +0,0 @@
-config = {"width": 100, "height": 50, "depth": 1}
-config.update({"height": 75, "depth": 3})
-area = config["width"] * config["height"] * config["depth"]
-print(area)
--- parity/03_shuffle.py
+++
@@ -1,6 +0,0 @@
-import random
-
-xs = [1, 2, 3, 4, 5]
-random.seed(0)
-random.shuffle(xs)
-print(xs)
