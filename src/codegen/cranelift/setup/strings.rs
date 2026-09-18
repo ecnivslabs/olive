616f3de8fa9b093e30db08676a610c0b525bef5f
vinceswu <vinceswu88@gmail.com>
2026-09-18 12:27:43 +0530

    Release rejected channel sends and null mutex unlocks

--- src/codegen/cranelift/setup/strings.rs
+++ src/codegen/cranelift/setup/strings.rs
@@ -316,6 +316,7 @@ impl<M: Module> CraneliftCodegen<M> {
             "__olive_list_repeat_typed" if args.len() == 2 => Some(0usize),
             "__olive_list_extend_typed" if args.len() == 2 => Some(1usize),
             "__olive_obj_update_typed" if args.len() == 2 => Some(1usize),
+            "__olive_chan_send" | "__olive_mutex_unlock" if args.len() == 2 => Some(1usize),
             "__olive_set_add_typed"
             | "__olive_set_remove_typed"
             | "__olive_set_contains_typed"
